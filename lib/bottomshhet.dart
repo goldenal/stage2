97b9d528f694110cb740b0b74b84d32f353ebce9
Adeosun adewale <goldenal86@gmail.com>
2021-08-20 23:00:24 +0100

    added bottom sheet

---
+++ lib/bottomshhet.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:hngig4intenship/controllers.dart';
+
+
+void showForgetPassword(BuildContext context){
+  showModalBottomSheet(context: context, builder: (context){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 60.0),
+      child: bottompage(),
+    );
+  });
+}
+
+
+
+
+
+class bottompage extends StatefulWidget {
+  const bottompage({Key? key}) : super(key: key);
+
+  @override
+  _bottompageState createState() => _bottompageState();
+}
+
+class _bottompageState extends State<bottompage> {
+  String name = nameController.text;
+  String stackt = stackController.text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text('Your name is: $name.'),
+          SizedBox(height: 30.0,),
+          Text('Your Stack is: $stackt.'),
+        ],
+      ),
+    );
+  }
+}
